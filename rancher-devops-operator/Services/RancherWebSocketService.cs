69aa266eade4894f0754de40babe2b3784bed22b
Jason van Eeden <jasonrve@gmail.com>
2025-11-27 18:57:16 +0200

    feat: Implement WebSocket authentication with token parsing and support for Basic/Bearer schemes

--- rancher-devops-operator/Services/RancherWebSocketService.cs
+++ rancher-devops-operator/Services/RancherWebSocketService.cs
@@ -63,6 +63,23 @@ public class RancherWebSocketService : BackgroundService
         // Get authentication token
         var token = await _authService.GetOrCreateTokenAsync(stoppingToken);
         
+        // Parse token to get username and password parts (format: token-xxxxx:yyyyyy)
+        string authHeader;
+        if (token.Contains(':'))
+        {
+            // Convert token to base64 for basic auth
+            var tokenBytes = System.Text.Encoding.UTF8.GetBytes(token);
+            var base64Token = Convert.ToBase64String(tokenBytes);
+            authHeader = $"Basic {base64Token}";
+        }
+        else
+        {
+            authHeader = $"Bearer {token}";
+        }
+        
+        // Configure WebSocket authentication
+        _webSocket.Options.SetRequestHeader("Authorization", authHeader);
+        
         // Allow insecure SSL if configured
         var allowInsecure = _configuration.GetValue<bool>("Rancher:AllowInsecureSsl", false);
         if (allowInsecure)
@@ -71,11 +88,11 @@ public class RancherWebSocketService : BackgroundService
         }
 
         // Build WebSocket URL - subscribe to namespace resource changes
-        // Rancher WebSocket API authenticates via query parameter, not Authorization header
         var wsUrl = _rancherUrl.Replace("https://", "wss://").Replace("http://", "ws://");
-        var subscribeUrl = $"{wsUrl}/v3/subscribe?eventNames=resource.change&resourceTypes=namespace&token={token}";
+        var subscribeUrl = $"{wsUrl}/v3/subscribe?eventNames=resource.change&resourceTypes=namespace";
 
-        _logger.LogInformation("Connecting to Rancher WebSocket: {Url}", subscribeUrl.Replace(token, "***"));
+        _logger.LogInformation("Connecting to Rancher WebSocket: {Url} with {AuthType}", 
+            subscribeUrl, token.Contains(':') ? "Basic" : "Bearer");
         
         await _webSocket.ConnectAsync(new Uri(subscribeUrl), stoppingToken);
         _logger.LogInformation("Connected to Rancher WebSocket");
